b86252d0d8dc1c0e4d32b2c048f4e6b040f2c7ae
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2018-08-27 16:59:49 +0200

    creation de la page /design pour la charte graphic

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :surveys
   devise_for :users
   root to: 'pages#home'
+  get '/design', to:'pages#design'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
