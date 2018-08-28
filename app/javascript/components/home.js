79bac93a0949b31eaed2e9f4a5ed836ab2de14d7
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2018-08-28 13:02:43 +0200

    animation home

---
+++ app/javascript/components/home.js
@@ -0,0 +1,17 @@
+function homeAnimation () {
+
+  const planet = document.querySelector('.planet');
+  console.log(planet);
+  const food = document.querySelector ('.deg0');
+  const transport = document.querySelector ('.deg180');
+  const trash = document.querySelector ('.deg90');
+  const house = document.querySelector ('.deg270');
+  planet.addEventListener('click', () => {
+    transport.classList.toggle('move');
+    food.classList.toggle('move');
+    trash.classList.toggle('move');
+    house.classList.toggle('move');
+  })
+}
+
+export {homeAnimation}
