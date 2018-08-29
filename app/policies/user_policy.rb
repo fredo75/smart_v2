354481ab9445dd6e390badf5f12a2e9903b0c9a5
frederic curier <frederic.curier@gmail.com>
2018-08-29 15:14:07 +0200

    config gem pundit done

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,37 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    record.user == user
+  end
+
+  def show?
+    return true
+  end
+
+  def new?
+    return true
+  end
+
+  def index?
+    return true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def edit?
+    record.user == user
+  end
+
+
+  def destroy?
+    record.user == user
+  end
+
+end
