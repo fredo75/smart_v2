b8f059efa4efdb6f78c677d10581079b9018533b
frederic curier <frederic.curier@gmail.com>
2018-08-30 16:29:33 +0200

    logic calcul done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,14 +17,16 @@ class UsersController < ApplicationController
     authorize @user
   end
 
-  def user_params
-    params.require(:user).permit(:mybio, :photo, :username)
-  end
 
   def show
+    # @user = User.find(params[:id])
     @user = current_user
-    # @users = policy_scope(User).order(created_at: :desc)
     authorize @user
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:mybio, :photo, :username)
+  end
 end
