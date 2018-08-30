94cdf281598b2b67543a288701fda0755f86b446
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2018-08-30 16:53:36 +0200

    graph js

--- app/javascript/components/chart.js
+++ app/javascript/components/chart.js
@@ -2,29 +2,64 @@ import Chart from 'chart.js'
 
 function testChart (){
   const c = document.getElementById('myChart');
+  const points = JSON.parse(c.dataset.points)
+  console.log(points);
+
   if (c) {
+    const ctx = c.getContext('2d');
+    const chart2 = new Chart(ctx, {
+      type: 'pie',
+      gridLines: {
+   display: false
+},
 
-  const ctx = c.getContext('2d');
-  const chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
+      options: {
+        scales:
+        { ticks: {
+            display: false
+          }
+        }
+      },
+      data: {
+        datasets : [{
+          label: "My Score",
+          backgroundColor: ['rgb(240, 25, 94)','rgb(246, 242, 171)','rgb(253, 172, 98)','rgb(255, 91, 93)'],
+          borderColor: 'rgba(250, 254, 187, 0)',
+          data: points
+        },{
+          backgroundColor: ['rgba(250, 254, 187, 0.8)','rgba(198, 239, 167, 0.7)','rgba(38, 107, 127, 0.6)','rgba(60, 176, 170, 0.5)'],
+          borderColor: 'rgb(250, 254, 187, 0 )',
+          data: [11],
+          backgroundColor: 'rgba(60, 176, 170, 0.5)'
 
-    // The data for our dataset
-    data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [{
-            label: "My First dataset",
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45],
         }]
-    },
+      }
+    });
 
-    // Configuration options go here
-    options: {}
-});
-  }
+   }
 
+  const c2 = document.getElementById('myChart2');
+  if (c2) {
+    const ctx = c2.getContext('2d');
+    const chart2 = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        datasets : [{
+          label: "My Score",
+          backgroundColor: ['rgb(240, 25, 94)','rgb(246, 242, 171)','rgb(253, 172, 98)','rgb(255, 91, 93)'],
+          borderColor: 'rgba(250, 254, 187, 0)',
+          data: [5, 11, 3, 7]
+        },{
+          backgroundColor: ['rgba(250, 254, 187, 0.8)','rgba(198, 239, 167, 0.7)','rgba(38, 107, 127, 0.6)','rgba(60, 176, 170, 0.5)'],
+          borderColor: 'rgb(250, 254, 187, 0 )',
+          data: [11],
+          backgroundColor: 'rgba(60, 176, 170, 0.5)'
+
+        }]
+      }
+    });
+
+   }
 }
 
 export {testChart};
