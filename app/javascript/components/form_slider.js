bc69e1404c0a2dda4a27a0c0573000e55f9bdb23
Camille Franc <Camille.franc.pro@gmail.com>
2018-08-30 19:39:10 +0200

    slider

--- app/javascript/components/form_slider.js
+++ app/javascript/components/form_slider.js
@@ -1,6 +1,10 @@
 function formSlider() {
   const nextButton1 = document.getElementById('step_1_button');
   const nextButton2 = document.getElementById('step_2_button');
+  const nextButton3 = document.getElementById('step_3_button');
+  const nextButton4 = document.getElementById('step_4_button');
+  const nextButton5 = document.getElementById('step_5_button');
+  const nextButton6 = document.getElementById('step_6_button');
   const step_1 = document.getElementById('step_1');
   const step_2 = document.getElementById('step_2');
   const step_3 = document.getElementById('step_3');
@@ -11,10 +15,22 @@ function formSlider() {
     nextButton1.addEventListener('click', (event) => {
       step_1.classList.add("hidden");
       step_2.classList.remove("hidden");
-    })
+    });
     nextButton2.addEventListener('click', (event) => {
       step_2.classList.add("hidden");
       step_3.classList.remove("hidden");
+    });
+    nextButton3.addEventListener('click', (event) => {
+      step_3.classList.add("hidden");
+      step_4.classList.remove("hidden");
+    });
+    nextButton4.addEventListener('click', (event) => {
+      step_4.classList.add("hidden");
+      step_5.classList.remove("hidden");
+    });
+    nextButton5.addEventListener('click', (event) => {
+      step_5.classList.add("hidden");
+      step_6.classList.remove("hidden");
     });
 }
 
