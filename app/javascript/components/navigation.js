d98145e4d3ed5b7820d91f75b0a4e80fe4aa05ec
Camille Franc <Camille.franc.pro@gmail.com>
2018-09-03 19:14:27 +0200

    navbar buttons

--- app/javascript/components/navigation.js
+++ app/javascript/components/navigation.js
@@ -1,6 +1,7 @@
 function openNavigation() {
   const earth = document.querySelector(".planet-navbar");
   earth.addEventListener('click', (event) => {
+    earth.classList.add("open");
     const buttons = document.querySelectorAll(".button-navbar");
     buttons.forEach((button) =>{
       button.classList.add("open");
